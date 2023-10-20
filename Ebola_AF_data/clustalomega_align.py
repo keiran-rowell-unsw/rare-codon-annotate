5c9c08be3378ad69c4322a64c0e7e09d8bcebd6c
Raph Caballes <138758354+z5344144@users.noreply.github.com>
2023-10-20 12:56:40 +1100

    Update clustalomega_align.py

--- Ebola_AF_data/clustalomega_align.py
+++ Ebola_AF_data/clustalomega_align.py
@@ -1,22 +1,20 @@
 from Bio import AlignIO
 from Bio.Align.Applications import ClustalOmegaCommandline
 from tqdm import tqdm
+import subprocess
 
 def perform_alignment(input_file, output_file):
     # Define the ClustalOmega command line
     clustalo_cline = ClustalOmegaCommandline(infile=input_file, outfile=output_file, verbose=True, auto=True)
     
-    # Start the ClustalOmega alignment
-    process = clustalo_cline()
+    # Start the ClustalOmega alignment as a subprocess
+    process = subprocess.Popen(str(clustalo_cline), shell=True)
     
     # Use tqdm to track the progress
     with tqdm(total=100, desc="Aligning", dynamic_ncols=True) as pbar:
-        for i in range(101):
+        while process.poll() is None:
             pbar.update(1)
             pbar.refresh()
-            process.poll()
-            if process.returncode is not None:
-                break
     
     # Load the alignment
     alignment = AlignIO.read(open(output_file), "clustal")
