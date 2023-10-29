0a73c41f9507fe7c3c24b1d51761ed175e890243
Raph Caballes <138758354+z5344144@users.noreply.github.com>
2023-10-30 09:54:44 +1100

    updating tree script

--- Ebola_AF_data/phylotree.py
+++ Ebola_AF_data/phylotree.py
@@ -1,29 +1,10 @@
 from Bio import AlignIO, SeqIO
-from Bio.Align.Applications import ClustalOmegaCommandline
 from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
 from Bio import Phylo
-from tqdm import tqdm
-import subprocess
 
-def perform_alignment(input_file, output_file):
-    # Define the ClustalOmega command line
-    clustalo_cline = ClustalOmegaCommandline(infile=input_file, outfile=output_file, verbose=True, auto=True)
-    
-    # Start the ClustalOmega alignment as a subprocess
-    process = subprocess.Popen(str(clustalo_cline), shell=True)
-    
-    # Use tqdm to track the progress
-    with tqdm(total=100, desc="Aligning", dynamic_ncols=True) as pbar:
-        while process.poll() is None:
-            pbar.update(1)
-            pbar.refresh()
-    
+def construct_tree(input_file, output_file):
     # Load the Clustalomega .aln output
-    clustal_alignment = AlignIO.read(output_file, "clustal")
-    
-    # Print the Clustalomega .aln alignment
-    print("Clustalomega .aln alignment:")
-    print(clustal_alignment)
+    clustal_alignment = AlignIO.read(input_file, "clustal")
     
     # Convert the output to MAF format
     maf_output_file = output_file + ".maf"
@@ -32,10 +13,6 @@ def perform_alignment(input_file, output_file):
     
     # Load the MAF alignment
     maf_alignment = AlignIO.read(maf_output_file, "maf")
-    
-    # Print the MAF alignment
-    print("\nMAF alignment:")
-    print(maf_alignment)
     
     # Calculate distances
     calculator = DistanceCalculator("identity")
@@ -54,4 +31,4 @@ def perform_alignment(input_file, output_file):
 input_file = input("Enter the input file name: ")
 output_file = input("Enter the output file name: ")
 
-perform_alignment(input_file, output_file)
+construct_tree(input_file, output_file)
